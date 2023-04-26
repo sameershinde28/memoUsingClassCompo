4d4e191d49288f314e74d57187171e7396343790
Sameer Shinde <130976046+sameershinde28@users.noreply.github.com>
2023-04-26 13:07:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+import React, { createRef } from "react";
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.inputRef = createRef();
+  }
+
+  componentDidMount() {
+    console.log((this.inputRef.current.value = "1000"));
+  }
+
+  getValue() {
+    console.log(this.inputRef.current.value);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ref in class component</h1>
+        <input type="text" ref={this.inputRef} />
+        <button onClick={() => this.getValue()}>click me</button>
+      </div>
+    );
+  }
+}
